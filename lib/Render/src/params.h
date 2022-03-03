3cdc833fdbe53fcc01a90ae8dbf51640d9b6cd59
Steven Cohen <peragwin@gmail.com>
2022-03-03 13:43:58 -0800

    fix compile

--- lib/Render/src/params.h
+++ lib/Render/src/params.h
@@ -2,7 +2,6 @@
 
 struct Render3_Params_t
 {
-    float diffGain;
     float warpScale, warpOffset, warpP, warpD, warpI;
     float scaleScale, scaleOffset, scaleP, scaleD, scaleI;
     float aspectX, aspectY;
